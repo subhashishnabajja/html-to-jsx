441a956f486648249c856ded57ed30d2b334365c
Subhashish Nabajja <subhashishnabajja619@gmail.com>
2021-08-17 18:39:03 +0530

    Add everything

---
+++ src/dataset.ts
@@ -0,0 +1,623 @@
+const attributes: { plain: string; jsx: string }[] = [
+  {
+    plain: "accept",
+    jsx: "accept",
+  },
+  {
+    plain: "accept-charset",
+    jsx: "acceptCharset",
+  },
+  {
+    plain: "accesskey",
+    jsx: "accessKey",
+  },
+  {
+    plain: "action",
+    jsx: "action",
+  },
+  {
+    plain: "align",
+    jsx: "align",
+  },
+  {
+    plain: "alt",
+    jsx: "alt",
+  },
+  {
+    plain: "async",
+    jsx: "async",
+  },
+  {
+    plain: "autocomplete",
+    jsx: "autoComplete",
+  },
+  {
+    plain: "autofocus",
+    jsx: "autoFocus",
+  },
+  {
+    plain: "autoplay",
+    jsx: "autoPlay",
+  },
+  {
+    plain: "bgcolor",
+    jsx: "bgColor",
+  },
+  {
+    plain: "border",
+    jsx: "border",
+  },
+  {
+    plain: "charset",
+    jsx: "charSet",
+  },
+  {
+    plain: "checked",
+    jsx: "checked",
+  },
+  {
+    plain: "cite",
+    jsx: "cite",
+  },
+  {
+    plain: "class",
+    jsx: "className",
+  },
+  {
+    plain: "color",
+    jsx: "color",
+  },
+  {
+    plain: "cols",
+    jsx: "cols",
+  },
+  {
+    plain: "colspan",
+    jsx: "colSpan",
+  },
+  {
+    plain: "content",
+    jsx: "content",
+  },
+  {
+    plain: "contenteditable",
+    jsx: "contentEditable",
+  },
+  {
+    plain: "controls",
+    jsx: "controls",
+  },
+  {
+    plain: "coords",
+    jsx: "coords",
+  },
+  {
+    plain: "datetime",
+    jsx: "dateTime",
+  },
+  {
+    plain: "default",
+    jsx: "default",
+  },
+  {
+    plain: "defer",
+    jsx: "defer",
+  },
+  {
+    plain: "dir",
+    jsx: "dir",
+  },
+  {
+    plain: "dirname",
+    jsx: "dirName",
+  },
+  {
+    plain: "disabled",
+    jsx: "disabled",
+  },
+  {
+    plain: "download",
+    jsx: "download",
+  },
+  {
+    plain: "draggable",
+    jsx: "draggable",
+  },
+  {
+    plain: "enctype",
+    jsx: "enctype",
+  },
+  {
+    plain: "for",
+    jsx: "htmlFor",
+  },
+  {
+    plain: "form",
+    jsx: "form",
+  },
+  {
+    plain: "formaction",
+    jsx: "formAction",
+  },
+  {
+    plain: "headers",
+    jsx: "headers",
+  },
+  {
+    plain: "height",
+    jsx: "height",
+  },
+  {
+    plain: "hidden",
+    jsx: "hidden",
+  },
+  {
+    plain: "high",
+    jsx: "high",
+  },
+  {
+    plain: "href",
+    jsx: "href",
+  },
+  {
+    plain: "hreflang",
+    jsx: "hrefLang",
+  },
+  {
+    plain: "http-equiv",
+    jsx: "",
+  },
+  {
+    plain: "id",
+    jsx: "id",
+  },
+  {
+    plain: "ismap",
+    jsx: "isMap",
+  },
+  {
+    plain: "kind",
+    jsx: "kind",
+  },
+  {
+    plain: "label",
+    jsx: "label",
+  },
+  {
+    plain: "lang",
+    jsx: "lang",
+  },
+  {
+    plain: "list",
+    jsx: "list",
+  },
+  {
+    plain: "loop",
+    jsx: "loop",
+  },
+  {
+    plain: "low",
+    jsx: "low",
+  },
+  {
+    plain: "max",
+    jsx: "max",
+  },
+  {
+    plain: "maxlength",
+    jsx: "maxLength",
+  },
+  {
+    plain: "media",
+    jsx: "media",
+  },
+  {
+    plain: "method",
+    jsx: "method",
+  },
+  {
+    plain: "min",
+    jsx: "min",
+  },
+  {
+    plain: "multiple",
+    jsx: "multiple",
+  },
+  {
+    plain: "muted",
+    jsx: "muted",
+  },
+  {
+    plain: "name",
+    jsx: "name",
+  },
+  {
+    plain: "novalidate",
+    jsx: "noValidate",
+  },
+  {
+    plain: "onabort",
+    jsx: "onAbort",
+  },
+  {
+    plain: "onblur",
+    jsx: "onBlur",
+  },
+  {
+    plain: "oncanplay",
+    jsx: "onCanPlay",
+  },
+  {
+    plain: "oncanplaythrough",
+    jsx: "onCanPlayThrough",
+  },
+  {
+    plain: "onchange",
+    jsx: "onChange",
+  },
+  {
+    plain: "onclick",
+    jsx: "onClick",
+  },
+  {
+    plain: "oncontextmenu",
+    jsx: "onContextMenu",
+  },
+  {
+    plain: "oncopy",
+    jsx: "onCopy",
+  },
+  {
+    plain: "oncuechange",
+    jsx: "onCueChange",
+  },
+  {
+    plain: "oncut",
+    jsx: "onCut",
+  },
+  {
+    plain: "ondblclick",
+    jsx: "onDoubleClick",
+  },
+  {
+    plain: "ondrag",
+    jsx: "onDrag",
+  },
+  {
+    plain: "ondragend",
+    jsx: "onDragEnd",
+  },
+  {
+    plain: "ondragenter",
+    jsx: "onDragEnter",
+  },
+  {
+    plain: "ondragleave",
+    jsx: "onDragLeave",
+  },
+  {
+    plain: "ondragover",
+    jsx: "onDrageOver",
+  },
+  {
+    plain: "ondragstart",
+    jsx: "onDragStart",
+  },
+  {
+    plain: "ondrop",
+    jsx: "onDrop",
+  },
+  {
+    plain: "ondurationchange",
+    jsx: "onDurationChange",
+  },
+  {
+    plain: "onemptied",
+    jsx: "onEmptied",
+  },
+  {
+    plain: "onended",
+    jsx: "onEnded",
+  },
+  {
+    plain: "onerror",
+    jsx: "onError",
+  },
+  {
+    plain: "onfocus",
+    jsx: "onFocus",
+  },
+  {
+    plain: "oninput",
+    jsx: "onInput",
+  },
+  {
+    plain: "oninvalid",
+    jsx: "onInvalid",
+  },
+  {
+    plain: "onkeydown",
+    jsx: "onKeyDown",
+  },
+  {
+    plain: "onkeypress",
+    jsx: "onKeyPress",
+  },
+  {
+    plain: "onkeyup",
+    jsx: "onKeyUp",
+  },
+  {
+    plain: "onload",
+    jsx: "onLoad",
+  },
+  {
+    plain: "onloadeddata",
+    jsx: "onLoadedData",
+  },
+  {
+    plain: "onloadedmetadata",
+    jsx: "onLoadedMetadata",
+  },
+  {
+    plain: "onloadstart",
+    jsx: "onLoadStart",
+  },
+  {
+    plain: "onmousedown",
+    jsx: "onMouseDown",
+  },
+  {
+    plain: "onmousemove",
+    jsx: "onMouseMove",
+  },
+  {
+    plain: "onmouseout",
+    jsx: "onMouseOut",
+  },
+  {
+    plain: "onmouseover",
+    jsx: "onMouseOver",
+  },
+  {
+    plain: "onmouseup",
+    jsx: "onMouseUp",
+  },
+  {
+    plain: "onmousewheel",
+    jsx: "onMouseWheel",
+  },
+  {
+    plain: "onpaste",
+    jsx: "onPaste",
+  },
+  {
+    plain: "onpause",
+    jsx: "onPause",
+  },
+  {
+    plain: "onplay",
+    jsx: "onPlay",
+  },
+  {
+    plain: "onplaying",
+    jsx: "onPlaying",
+  },
+  {
+    plain: "onprogress",
+    jsx: "onProgress",
+  },
+  {
+    plain: "onratechange",
+    jsx: "onRateChange",
+  },
+  {
+    plain: "onreset",
+    jsx: "onReset",
+  },
+  {
+    plain: "onresize",
+    jsx: "onResize",
+  },
+  {
+    plain: "onscroll",
+    jsx: "onScroll",
+  },
+  {
+    plain: "onsearch",
+    jsx: "onSearch",
+  },
+  {
+    plain: "onseeked",
+    jsx: "onSeeked",
+  },
+  {
+    plain: "onseeking",
+    jsx: "onSeeking",
+  },
+  {
+    plain: "onselect",
+    jsx: "onSelect",
+  },
+  {
+    plain: "onstalled",
+    jsx: "onStalled",
+  },
+  {
+    plain: "onsubmit",
+    jsx: "onSubmit",
+  },
+  {
+    plain: "onsuspend",
+    jsx: "onSuspend",
+  },
+  {
+    plain: "ontimeupdate",
+    jsx: "onTimeUpdate",
+  },
+  {
+    plain: "ontoggle",
+    jsx: "onToggle",
+  },
+  {
+    plain: "onunload",
+    jsx: "onUnload",
+  },
+  {
+    plain: "onvolumechange",
+    jsx: "onVolumeChage",
+  },
+  {
+    plain: "onwaiting",
+    jsx: "onWaiting",
+  },
+  {
+    plain: "onwheel",
+    jsx: "onWheel",
+  },
+  {
+    plain: "open",
+    jsx: "open",
+  },
+  {
+    plain: "optimum",
+    jsx: "optimum",
+  },
+  {
+    plain: "pattern",
+    jsx: "pattern",
+  },
+  {
+    plain: "placeholder",
+    jsx: "placeHolder",
+  },
+  {
+    plain: "poster",
+    jsx: "poster",
+  },
+  {
+    plain: "preload",
+    jsx: "preload",
+  },
+  {
+    plain: "readonly",
+    jsx: "readOnly",
+  },
+  {
+    plain: "rel",
+    jsx: "rel",
+  },
+  {
+    plain: "required",
+    jsx: "required",
+  },
+  {
+    plain: "reversed",
+    jsx: "reversed",
+  },
+  {
+    plain: "rows",
+    jsx: "rows",
+  },
+  {
+    plain: "rowspan",
+    jsx: "rowSpan",
+  },
+  {
+    plain: "sandbox",
+    jsx: "sandbox",
+  },
+  {
+    plain: "scope",
+    jsx: "scope",
+  },
+  {
+    plain: "selected",
+    jsx: "selected",
+  },
+  {
+    plain: "shape",
+    jsx: "shape",
+  },
+  {
+    plain: "size",
+    jsx: "size",
+  },
+  {
+    plain: "sizes",
+    jsx: "sizes",
+  },
+  {
+    plain: "span",
+    jsx: "span",
+  },
+  {
+    plain: "spellcheck",
+    jsx: "spellCheck",
+  },
+  {
+    plain: "src",
+    jsx: "src",
+  },
+  {
+    plain: "srcdoc",
+    jsx: "srcDoc",
+  },
+  {
+    plain: "srclang",
+    jsx: "srcLang",
+  },
+  {
+    plain: "srcset",
+    jsx: "srcSet",
+  },
+  {
+    plain: "start",
+    jsx: "start",
+  },
+  {
+    plain: "step",
+    jsx: "step",
+  },
+  {
+    plain: "style",
+    jsx: "style",
+  },
+  {
+    plain: "tabindex",
+    jsx: "tabIndex",
+  },
+  {
+    plain: "target",
+    jsx: "target",
+  },
+  {
+    plain: "title",
+    jsx: "title",
+  },
+  {
+    plain: "translate",
+    jsx: "translate",
+  },
+  {
+    plain: "type",
+    jsx: "type",
+  },
+  {
+    plain: "usemap",
+    jsx: "useMap",
+  },
+  {
+    plain: "value",
+    jsx: "value",
+  },
+  {
+    plain: "width",
+    jsx: "width",
+  },
+  {
+    plain: "wrap",
+    jsx: "wrap",
+  },
+];
+export default attributes;
--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,65 @@
 import * as vscode from "vscode";
+import matchAll = require("string.prototype.matchall");
+import attributes from "./dataset";
+
+//Handle the conversion
+const handleConversion = () => {
+  //The active text editor
+  const editor = vscode.window.activeTextEditor;
+  //The selection
+  const selection = vscode.window.activeTextEditor
+    ?.selection as vscode.Selection;
+  //The edited block
+  const newBlock: string[] = [];
+  //Loop through multiple selections
+
+  const start = selection.start; //start of the selection
+  const end = selection.end; //end of the selection
+  //Selection is empty
+  if (start.character === end.character && start.line === end.line) {
+    vscode.window.showErrorMessage("Selection is empty");
+  }
+  //Selection Range
+  const selectionRange = new vscode.Range(start, end);
+
+  //Get the selection text
+  const textBlock = vscode.window.activeTextEditor?.document
+    .getText(selectionRange)
+    .split("\n") as string[];
+  console.log("start of the text");
+
+  //Loop through each line of the selection
+  for (const line of textBlock) {
+    const regex =
+      /([^\r\n\t\f\v= '"]+)(?:=(["'])?((?:.(?!\2?\s+(?:\S+)=|\2))+.)\2?)?/g;
+    const matches = [...matchAll(line, regex)];
+    let newLine: string = "";
+
+    for (const match of matches) {
+      const attribute = attributes.find(({ plain }) => {
+        return plain === match[1];
+      });
+      if (attribute) {
+        newLine = line.replace(attribute?.plain, attribute?.jsx);
+      }
+    }
+    newBlock.push(newLine);
+  }
+  //Edit the block in the editor
+  editor?.edit((editorBuilder: vscode.TextEditorEdit) => {
+    editorBuilder.replace(selection, newBlock.join("\n"));
+  });
+};
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "html-to-jsx" is now active!');
-
   let disposable = vscode.commands.registerCommand(
     "html-to-jsx.convertToJsx",
-    () => {}
+    handleConversion
   );
 
   context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() {
+  vscode.window.showInformationMessage("Successfully disable");
+}
